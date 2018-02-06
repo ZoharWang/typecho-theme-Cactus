5adff0e5f16ff73261118c47e4d96a2226e4e163
Zohar Wang <zoharwang@gmail.com>
2018-02-07 02:43:16 +0800

    :sparkles: 支持添加ICP备案许可证号.

--- footer.php
+++ footer.php
@@ -4,6 +4,9 @@
 					&copy; Copyright <?php echo date('Y'); ?> <a href="<?php $this->options->siteUrl(); ?>"><?php $this->options->title() ?></a> /
 					<a href="https://www.iwch.me/archives/668.html">Cactus</a> /
 					<a href="http://typecho.org">Typecho</a>
+<?php if ($this->options->icpNumber) : ?>
+					/ <a href="http://www.miibeian.gov.cn/" target="_blank" rel="nofollow"><?php $this->options->icpNumber(); ?></a>
+<?php endif; ?>
 				</p>
 			</footer>
 		</section>
--- functions.php
+++ functions.php
@@ -4,4 +4,7 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 function themeConfig($form) {
 	$avatarUrl = new Typecho_Widget_Helper_Form_Element_Text('avatarUrl', NULL, NULL, _t('头像 URL 地址'), _t('在这里填入一个图片 URL 地址, 留空则返回 Gravatar, 不建议留空.'));
 	$form->addInput($avatarUrl);
+
+	$icpNumber = new Typecho_Widget_Helper_Form_Element_Text('icpNumber', NULL, NULL, _t('ICP 备案许可证号'), _t('在这里填入一个 ICP 备案许可证号, 留空则不显示.'));
+	$form->addInput($icpNumber);
 }
